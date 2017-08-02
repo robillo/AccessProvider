62e251169d1f5670510dd73c95ed363c50366cec
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-08-02 21:01:03 +0530

    functional code complete except getContacts()

--- app/src/main/java/com/robillo/accessprovider/MainActivity.java
+++ app/src/main/java/com/robillo/accessprovider/MainActivity.java
@@ -1,15 +1,20 @@
 package com.robillo.accessprovider;
 
+import android.app.LoaderManager;
 import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor>{
 
     Button show, delete, find, add;
     EditText idDelete, idFind, nameAdd;
@@ -34,6 +39,54 @@ public class MainActivity extends AppCompatActivity {
         nameAdd = (EditText) findViewById(R.id.name_add);
         results = (TextView) findViewById(R.id.results);
 
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                getContacts();
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String deleteId = idDelete.getText().toString();
+                long id = contentResolver.delete(CONTENT_URI, "id = ?", new String[]{deleteId});
+//                getContacts();
+            }
+        });
+
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String addId = idDelete.getText().toString();
+                String[] projection = {"id", "name"};
+                Cursor cursor = contentResolver.query(CONTENT_URI, projection,  "id = ?", new String[]{addId}, null);
+                String mContact = "";
+                //noinspection ConstantConditions
+                if(cursor.moveToFirst()){
+                    do{
+                        String id = cursor.getString(cursor.getColumnIndex("id"));
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        mContact = mContact + id + " : " + name + "\n";
+                    }while (cursor.moveToFirst());
+                }
+                cursor.close();
+            }
+        });
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameToAdd = nameAdd.getText().toString();
+                ContentValues values = new ContentValues();
+                values.put("name", nameToAdd);
+                contentResolver.insert(CONTENT_URI, values);
+
+//                getContacts();
+            }
+        });
+
+//        getLoaderManager().initLoader(1, null, this);
 //        getContacts();
     }
 
@@ -41,14 +94,39 @@ public class MainActivity extends AppCompatActivity {
         String[] projection = new String[]{"id", "name"};
         Cursor cursor = contentResolver.query(CONTENT_URI, projection, null, null, null);
         String mContactsList = "";
+        //noinspection ConstantConditions
         if(cursor.moveToFirst()){
             do{
                 String id = cursor.getString(cursor.getColumnIndex("id"));
                 String name = cursor.getString(cursor.getColumnIndex("name"));
-                mContactsList = id + " : " + name + "\n";
+                mContactsList = mContactsList + id + " : " + name + "\n";
             }while (cursor.moveToFirst());
         }
         cursor.close();
         results.setText(mContactsList);
+    }
+
+    @Override
+    public android.content.Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        return new CursorLoader(this, CONTENT_URI, null, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(android.content.Loader<Cursor> loader, Cursor cursor) {
+        String mContactsList = "";
+        if(cursor.moveToFirst()){
+            do{
+                String id = cursor.getString(cursor.getColumnIndex("id"));
+                String name = cursor.getString(cursor.getColumnIndex("name"));
+                mContactsList = mContactsList + id + " : " + name + "\n";
+            }while (cursor.moveToFirst());
+        }
+        cursor.close();
+        results.setText(mContactsList);
+    }
+
+    @Override
+    public void onLoaderReset(android.content.Loader<Cursor> loader) {
+
     }
 }
